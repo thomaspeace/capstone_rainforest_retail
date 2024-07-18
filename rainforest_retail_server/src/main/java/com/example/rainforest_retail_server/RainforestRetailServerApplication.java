183baab812650a8735426cb70120d0f313938043
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-18 09:42:26 +0100

    add server

---
+++ rainforest_retail_server/src/main/java/com/example/rainforest_retail_server/RainforestRetailServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rainforest_retail_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainforestRetailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainforestRetailServerApplication.class, args);
+	}
+
+}
---
+++ rainforest_retail_server/src/test/java/com/example/rainforest_retail_server/RainforestRetailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rainforest_retail_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainforestRetailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
