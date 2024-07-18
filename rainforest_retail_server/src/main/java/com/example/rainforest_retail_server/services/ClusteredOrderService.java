8d764d21f2f3738d8d46fed5f5dd35421f784439
KhanZuh <khanzuhair96@live.co.uk>
2024-07-18 17:06:57 +0100

    Fixed random init bug

--- rainforest_retail_server/src/main/java/com/example/rainforest_retail_server/services/ClusteredOrderService.java
+++ rainforest_retail_server/src/main/java/com/example/rainforest_retail_server/services/ClusteredOrderService.java
@@ -11,6 +11,8 @@ import com.example.rainforest_retail_server.repositories.VanRepository;
 import jakarta.transaction.Transactional;
 import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
+import org.apache.commons.math3.random.JDKRandomGenerator;
+import org.apache.commons.math3.random.RandomGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,28 +35,46 @@ public class ClusteredOrderService {
         return clusteredOrderRepository.findAll();
     }
 
-    // we would need to change 1
     @Transactional
-    public void createCluster(long regionalHubId){
+    public void createCluster(long regionalHubId) {
+        // Fetch orders that are not delivered for the given regional hub
         List<Order> listOfOrders = new ArrayList<>(orderRepository.findByRegionalHubIdEqualsAndDeliveryStatusEquals(regionalHubId, DeliveryStatus.NOT_DELIVERED));
 
+        // Wrap orders for clustering
         List<OrderWrapper> clusterInput = new ArrayList<>(listOfOrders.size());
         for (Order order : listOfOrders) {
             clusterInput.add(new OrderWrapper(order));
         }
 
-        List<Van> listOfVans = new ArrayList<>(vanRepository.findByRegionalHubIdEquals(1));
+        // Fetch vans for the regional hub
+        List<Van> listOfVans = new ArrayList<>(vanRepository.findByRegionalHubIdEquals(regionalHubId));
 
-        KMeansPlusPlusClusterer<OrderWrapper> clusterer = new KMeansPlusPlusClusterer<>(listOfVans.size(), 10000);
+        // Create a random generator with a fixed seed
+        RandomGenerator random = new JDKRandomGenerator();
+        random.setSeed(42); // You can choose any integer as the seed
+
+        // Create the clusterer with the fixed random generator
+        KMeansPlusPlusClusterer<OrderWrapper> clusterer = new KMeansPlusPlusClusterer<>(
+                listOfVans.size(),
+                10000,
+                new org.apache.commons.math3.ml.distance.EuclideanDistance(),
+                random
+        );
+
+        // Perform clustering
         List<CentroidCluster<OrderWrapper>> clusterResults = clusterer.cluster(clusterInput);
 
+        // Process clustering results
         for (int i = 0; i < clusterResults.size(); i++) {
             Van vanForCluster = listOfVans.get(i);
-            List<Order> clusterOfOrders= new ArrayList<>();
+            List<Order> clusterOfOrders = new ArrayList<>();
+
             for (OrderWrapper orderWrapper : clusterResults.get(i).getPoints()) {
                 Order orderForCluster = orderWrapper.getOrder();
                 clusterOfOrders.add(orderForCluster);
             }
+
+            // Create and save clustered order
             ClusteredOrder clusteredOrder = new ClusteredOrder(clusterOfOrders, vanForCluster.getRegionalHub(), vanForCluster);
             clusteredOrderRepository.save(clusteredOrder);
 
