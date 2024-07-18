1c19b065883fce984d8acf815213de04fe6dfaa4
KhanZuh <khanzuhair96@live.co.uk>
2024-07-18 10:18:56 +0100

    Van model done

---
+++ rainforest_retail_server/src/main/java/com/example/rainforest_retail_server/Models/Van.java
@@ -0,0 +1,38 @@
+package com.example.rainforest_retail_server.Models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "vans")
+public class Van {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "regional_hub_id")
+    private RegionalHub regionalHub;
+
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RegionalHub getRegionalHub() {
+        return regionalHub;
+    }
+
+    public void setRegionalHub(RegionalHub regionalHub) {
+        this.regionalHub = regionalHub;
+    }
+}
+
+
