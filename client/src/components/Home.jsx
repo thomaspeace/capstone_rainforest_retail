068d3b1f563f1069b4495b576281c7e0f6f28e78
KhanZuh <khanzuhair96@live.co.uk>
2024-07-22 16:15:55 +0100

    logo added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,16 @@
-
+import React from 'react';
+import logo from '../assets/Logo.png';
 
 const Home = () => {
-
-    return(
-    <div>
-        <h1>Welcome Dashboard</h1>
-        <p>This is the home page of our delivery managemnt system</p>
-    </div>
-    )
+    return (
+        <div className="home-container">
+            <div className="logo-container">
+                <img src={logo} alt="Rainforest Retail Logo" className="logo" />
+            </div>
+            <h2>Welcome to the Dashboard</h2>
+            <p>This is the home page of our delivery management system</p>
+        </div>
+    );
 };
-
-
-
 
 export default Home;
