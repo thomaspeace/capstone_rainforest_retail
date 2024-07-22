73ee2a6666438c5ee29887d53a524b67fcfe9508
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-22 16:37:17 +0100

    added cool dashboard

--- client/src/components/RegionCarousel.jsx
+++ client/src/components/RegionCarousel.jsx
@@ -1,33 +1,38 @@
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/RegionCarousel.css';
+import { Button } from 'react-bootstrap';
 
 const RegionCarousel = () => {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <ExampleCarouselImage text="First region" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+    <Carousel className='carousel-class'>
+      <Carousel.Item interval={4000} className="carousel-item">
+        <div className="carousel-background">
+          <Carousel.Caption>
+            <h3>London</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <ExampleCarouselImage text="Second region" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+      <Carousel.Item interval={4000} className="carousel-item">
+        <div className="carousel-background">
+          <Carousel.Caption>
+            <h3>South West</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <ExampleCarouselImage text="Third region" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+      <Carousel.Item interval={4000} className="carousel-item">
+        <div className="carousel-background">
+          <Carousel.Caption>
+            <h3>Midlands</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
-}
+};
 
 export default RegionCarousel;
