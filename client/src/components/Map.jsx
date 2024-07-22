ab61fc53860d7c95dc5079d383f39df70c07d75b
Sunny Mudhar <sunielmudhar@gmail.com>
2024-07-22 14:31:32 +0100

    partial route functionality

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,14 +1,43 @@
-import { useRef , useEffect } from "react";
+import { useRef , useEffect, useState } from "react";
 import TT_API from "../utils/TT_API";
 
 const Map = () => {
-
+    const [map, setMap] = useState(null);
     const mapElement = useRef();
 
     useEffect(() => {
-        const map = TT_API.getMAP(mapElement);
-        return map;
+        const tt_map = TT_API.getMAP(mapElement);
+        setMap(tt_map);
+        planRoute(tt_map);
     }, []);
+
+    const planRoute = async (mapInstance) => {
+        const waypoints = [
+            { lng: 4.899, lat: 52.372 }, // Amsterdam
+            { lng: 4.479, lat: 51.922 }, // Rotterdam
+            { lng: 5.121, lat: 52.090 }  // Utrecht
+        ];
+        const geo = await TT_API.getROUTE(waypoints);
+
+        if(geo) {
+            mapInstance.addLayer({
+                id: "route",
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data: geo,
+                },
+                paint: {
+                    "line-color": "#00d7ff",
+                    "line-width": 8,
+                },
+            })
+        }
+
+        const bounds = TT_API.getBOUNDS();
+
+        map.fitBounds(bounds, {padding: 20})
+    }
 
     return (
         <div ref={mapElement}></div>
--- client/src/utils/TT_API.jsx
+++ client/src/utils/TT_API.jsx
@@ -1,18 +1,40 @@
 import * as tt from "@tomtom-international/web-sdk-maps";
+import ttServices from "@tomtom-international/web-sdk-services";
 
 const VITE_TOMTOM_API = import.meta.env.VITE_TOMTOM_API;
+const VITE_TOMTOM_URL = import.meta.env.VITE_TOMTOM_URL;
+
+let geojson;
 
 export default {
     getMAP: (mapElement) => {
         let routeMap = tt.map({
             key: VITE_TOMTOM_API,
-            container: mapElement.current
+            container: mapElement.current,
+            center: [4.899, 52.372],
+            zoom: 10
         });
 
         return () => {
             if (routeMap) {
-                routeMap.remove();
+                 routeMap.remove;
             }
         }
+    },
+    getROUTE: (waypoints) => {
+        ttServices.services.calculateRoute({
+            key: VITE_TOMTOM_API,
+            locations: waypoints.map(point => `${point.lng},${point.lat}`).join(':')
+        }).then(response => {
+            geojson = response.toGeoJson();
+            return geojson.features;
+        })
+    },
+    getBOUNDS: () => {
+        let bounds = new tt.LngLatBounds();
+        geojson.features[0].geometry.coordinates.forEach(point => {
+            bounds.extend(tt.LngLat.convert(point))
+        })
+        return bounds;
     }
 }
