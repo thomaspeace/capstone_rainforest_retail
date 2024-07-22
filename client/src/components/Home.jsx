d3a6e7fdf2990550ed29a72837c33d5281c97c0b
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-22 13:34:32 +0100

    add bootstrap on home and carousel

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,33 @@
+import { Row, Container, Col } from "react-bootstrap";
+import './styles/Home.css';
+import RegionCarousel from "./RegionCarousel";
 
 
 const Home = () => {
 
     return(
-    <div>
-        <h1>Welcome Dashboard</h1>
-        <p>This is the home page of our delivery managemnt system</p>
-    </div>
+        <Container>
+            <div className="home">
+                <Row>
+                    <Col className="col-12">
+                        <h1>Rainforest Retail</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={8}>
+                    <p>Text</p>
+                    </Col>
+                    <Col xs={12} md={4}>
+                    <p>Text</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <RegionCarousel/>
+                </Row>
+                <p>This is the home page of our delivery managemnt system</p>
+            </div>
+        </Container>
+
     )
 };
 
