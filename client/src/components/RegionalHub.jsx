bd71dda793146e28ff359f5bf5e78239ca762bd7
KhanZuh <khanzuhair96@live.co.uk>
2024-07-24 16:50:55 +0100

    changes to footer and regionalHub

--- client/src/components/RegionalHub.jsx
+++ client/src/components/RegionalHub.jsx
@@ -21,7 +21,6 @@ const RegionalHub = ({ handleGetCluster }) => {
                 }
                 const data = await response.json();
                 setHub(data);
-                setError(null);
             } catch (error) {
                 console.error("Error fetching regional hub:", error);
             } finally {
@@ -72,7 +71,7 @@ const RegionalHub = ({ handleGetCluster }) => {
                         <Card.Body>
                             <p>Total Orders: {hub.orders ? hub.orders.length : 0}</p>
                             <p>Delivered: {hub.orders ? hub.orders.filter(order => order.deliveryStatus === 'DELIVERED').length : 0}</p>
-                            <p>In Transit: {hub.orders ? hub.orders.filter(order => order.deliveryStatus === 'OUT_FOR_DELIVERY').length : 0}</p>
+                            {/* <p>In Transit: {hub.orders ? hub.orders.filter(order => order.deliveryStatus === 'OUT_FOR_DELIVERY').length : 0}</p> */}
                             <p>Pending: {hub.orders ? hub.orders.filter(order => order.deliveryStatus === 'NOT_DELIVERED').length : 0}</p>
                         </Card.Body>
                     </Card>
